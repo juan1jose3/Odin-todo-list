372c77124ed7fae7d676fce4a8421f0d17b1989b
juan1jose3 <moralesbenitezjuanjose@gmail.com>
2026-09-21 16:16:06 -0500

    configuring webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+import path from "node:path";
+import { fileURLToPath } from 'node:url';
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default{
+    mode: "development",
+    entry: "./src/index.js",
+    output:{
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
